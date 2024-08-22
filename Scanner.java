7d3919fdeb3e39a7472c1a7060dfd7167298ddb4
Bruno Rocha Guimarães <88166479+Bruno-BRG@users.noreply.github.com>
2024-08-21 22:03:09 -0300

    update

--- Scanner.java
+++ Scanner.java
@@ -62,10 +62,62 @@ class Scanner {
 			case '*':
 				addToken(STAR);
 				break;
+			case '!':
+				addToken(match('=') ? BANG_EQUAL : BANG);
+				break;
+			case '=':
+				addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+				break;
+			case '<':
+				addToken(match('=') ? LESS_EQUAL : LESS);
+				break;
+			case '>':
+				addToken(match('=') ? GREATER_EQUAL : GREATER);
+				break;
+			case '/':
+				if (match('/')) {
+					while (peek() != '\n' && !isAtEnd())
+						advance();
+				} else {
+					addToken(SLASH);
+				}
+				break;
+
+			case ' ':
+			case '\r':
+			case '\t':
+				break;
+
+			case '\n':
+				line++;
+				break;
+
+			case '"':
+				string();
+				break;
+
 			default:
 				Lox.error(line, "Unexpected character.");
 				break;
 		}
+	}
+
+	private void string() {
+		while (peek() != '"' && !isAtEnd()) {
+			if (peek() == '\n')
+				line++;
+			advance();
+		}
+
+		if (isAtEnd()) {
+			Lox.error(line, "Unterminated string.");
+			return;
+		}
+
+		advance();
+
+		String value = source.substring(start + 1, current - 1);
+		addToken(STRING, value);
 	}
 
 	private boolen isAtEnd() {
@@ -84,5 +136,21 @@ class Scanner {
 	private void addToken(TokenType type, Object literal) {
 		String text = source.substring(start, current);
 		tokens.add(new Token(type, text, literal, line));
+	}
+
+	private boolean match(char expected) {
+		if (isAtEnd())
+			return false;
+		if (source.charAt(current) != expected)
+			return false;
+
+		current++;
+		return true;
+	}
+
+	private char peek() {
+		if (isAtEnd())
+			return '\0';
+		return source.charAt(current);
 	}
 }
